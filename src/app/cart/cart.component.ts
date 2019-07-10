1458bfd2457f32e526dc504da3bd588f436cca9c
Daniel Bell <dan@bolt.com>
2019-07-09 21:29:33 -0700

    Bolt Support Eng Case Study: Removed comment

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { hasParentInjector } from '@angular/core/src/render3/util';
-// import '../../assets/deprecated_js';
 
 @Component({
   selector: 'app-cart',
