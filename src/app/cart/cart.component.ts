182702d185614f193972d247ee61dbb1f0ced7ec
Harshil <hashah92.usa@gmail.com>
2019-07-12 21:07:18 -0400

    Bolt Support Eng Case #2
    Added the alert message to alert users to check the terms checkbox if haven't to proceed further.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -13,11 +13,9 @@ export class CartComponent implements OnInit {
   affirm:boolean=true;
   isTermsAccepted:boolean=false;
   constructor() { }
-
   ngOnInit() {
     //This would be a good place to initialize DOM Variables and/or functions
   }
-
   boltCheckout(){
     //Here we will check if the terms checkbox is checked againsts the isTermsAccepted variable that we default initialized to false above.
     if(this.isTermsAccepted)
@@ -25,7 +23,6 @@ export class CartComponent implements OnInit {
       this.bolt = !this.bolt;
     }
   }
-
   giftCheckout(){
     //COMPLETE THIS FUNCTION
     // This function toggles the Gift Card overlay
@@ -34,7 +31,6 @@ export class CartComponent implements OnInit {
     this.gift = !this.gift;
     }
   }
-
   paypalCheckout(){
     //COMPLETE THIS FUNCTION
     // This function toggles the paypal overlay
@@ -43,7 +39,6 @@ export class CartComponent implements OnInit {
     this.paypal = !this.paypal;
     }
   }
-
   affirmCheckout(){
     //COMPLETE THIS FUNCTION
     // This function toggles the paypal overlay
@@ -52,25 +47,15 @@ export class CartComponent implements OnInit {
     this.affirm = !this.affirm;
     }
   }
-
   termsFunc(){
     //COMPLETE THIS FUNCTION
     //This function is ran when a user selects the final sale checkbox
     this.isTermsAccepted = !this.isTermsAccepted;
   }
-
-  enableCheckout(){
-    //COMPLETE THIS FUNCTION
-    // This function enables the checkout buttons when a condition is met
-  }
-
-  disableCheckout(){
-    //COMPLETE THIS FUNCTION
-    // This disables the checkout buttons when a condition isn't met
-  }
-
-  alertUser(){
-    //COMPLETE THIS FUNCTION
-    // This function needs to alert the user when try to click one of the checkout buttons without agreeing to the terms.
-  }
+  alertUser(){ 
+    if(this.isTermsAccepted==false)
+    {
+    alert("Please check the terms to proceed");
+    }
+  }    
 }
